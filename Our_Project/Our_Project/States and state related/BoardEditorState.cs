b7cf54ea89dc1df503eb03c439acd338da35c5c3
ShacharBartal <shacharbartal@gmail.com>
2019-08-11 09:41:44 +0300

    Added the classes Shape and Board Editor(s), just with constractor for the moment. Did there interface. Didnt wrote button who call to level editor

---
+++ Our_Project/Our_Project/States and state related/BoardEditorState.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XELibrary;
+
+namespace Our_Project.States_and_state_related
+{
+    public sealed class BoardEditorState : BaseGameState , IBoardEditorState
+    {
+        private ISoundManager soundEffect;
+        
+        public BoardEditorState(Game game) : base(game)
+        {
+            game.Services.AddService(typeof(IBoardEditorState), this);
+
+            soundEffect = (ISoundManager)game.Services.GetService(typeof(ISoundManager));
+
+        }
+
+    }
+}
