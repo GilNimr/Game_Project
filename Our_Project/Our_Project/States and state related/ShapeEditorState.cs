71f1ca62a7a93aa9b1309b9fc2c2655bbf1b2305
ShacharBartal <shacharbartal@gmail.com>
2019-08-11 10:51:45 +0300

    Added few variables to BoardEditor

--- Our_Project/Our_Project/States and state related/ShapeEditorState.cs
+++ Our_Project/Our_Project/States and state related/ShapeEditorState.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XELibrary;
 
 namespace Our_Project.States_and_state_related
 {
-    class ShapeEditorState
+    public sealed class ShapeEditorState : BaseGameState, IShapeEditorState
     {
+        private ISoundManager soundEffect;
+
+
+        public ShapeEditorState(Game game) : base(game)
+        {
+            game.Services.AddService(typeof(IShapeEditorState), this);
+
+            soundEffect = (ISoundManager)game.Services.GetService(typeof(ISoundManager));
+
+        }
+
+
     }
 }
